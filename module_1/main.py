9a00aad22c88e1c128cfd18490e18f6330a74deb
maria <mariia.logacheva99@gmail.com>
2024-12-09 18:20:36 +0300

    09.12

---
+++ module_1/main.py
@@ -0,0 +1,10 @@
+home_work = 12
+
+time_of_work = 1.5
+
+name_course = 'Python'
+
+time_task = 1.5 / 12
+
+print('Курс:', name_course, ',' 'всего задач:', home_work, ',' 'затрачено часов:'
+      , time_of_work, ',' 'среднее время выполнения:', time_task, 'часа' '.')
