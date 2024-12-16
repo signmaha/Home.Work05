08ce8ba5046b0692ff4355bf94520c43c132e52f
maria <mariia.logacheva99@gmail.com>
2024-12-16 22:26:22 +0300

    16.12

--- modul_5/attributes_methods_object.py
+++ modul_5/attributes_methods_object.py
@@ -10,11 +10,27 @@ class House:
         if new_floor > self.number_of_floors or new_floor < self.number_of_floors:
             print(f'{new_floor + 1} такого этажа не существует')
 
+    def __len__(self):
+        return self.number_of_floors
 
-h1 = House('ЖК Горский', 18)
+    def __str__(self):
+        return f"Название: {self.name}, колличество этажей: {self.number_of_floors}"
 
-h2 = House('Домик в деревне', 2)
 
-h1.go_to(5)
 
-h2.go_to(10)
+#h1 = House('ЖК Горский', 18)
+#h2 = House('Домик в деревне', 2)
+#h1.go_to(5)
+#h2.go_to(10)
+
+h1 = House('ЖК Эльбрус', 10)
+
+h2 = House('ЖК Акация', 20)
+
+print(h1)
+
+print(h2)
+
+print(len(h1))
+
+print(len(h2))
